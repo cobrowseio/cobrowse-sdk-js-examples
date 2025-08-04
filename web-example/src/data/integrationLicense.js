15c882964ea8b38d4483ce169b683c614fc8af09
Dani <hello@dyon.me>
2025-08-04 13:18:22 +0200

    feat: add Genesys messaging integration

--- web-example/src/data/integrationLicense.js
+++ web-example/src/data/integrationLicense.js
@@ -1,5 +1,6 @@
 export const licenses = {
   genesys: 'FJ2BXZIU_iEE_Q',
+  'genesys-messaging': 'FJ2BXZIU_iEE_Q',
   zendesk: 'MECLNs3JUpHRGg',
   freshchat: 'GOz3faJSJQ3L9g',
   salesforce: 'IKIC3uqyhRJrYg',
